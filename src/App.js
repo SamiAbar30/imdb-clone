02400086763739f406c7ead7329ccc8c54851916
SamiAbar30 <samiabar30@gmail.com>
2023-01-25 17:54:08 +0100

    setup project v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import ImdbLogo from "./assets/images/Imdb-logo.png"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='w-full h-screen'>
+			<div className='flex items-center justify-around w-full h-20 bg-black'>
+				<div className="">
+					<img src={ImdbLogo} alt='logo' className="h-10"/>
+				</div>
+				<div className=''>
+					<input type='text' className='p-2 rounded-lg' />
+					<span className='rounded-lg bg-yellow-300'></span>
+				</div>
+				<div>
+					<button className='px-4 py-2 text-white border-2 border-yellow-300 rounded-lg '>
+						watchlist
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
